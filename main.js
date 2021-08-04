8ac4d3a75e4fb49d5428bc1d26b2cc72c79ebeaa
YafitSamuel <gyafit642@gmail.com>
2021-08-05 00:31:15 +0300

    commit

---
+++ main.js
@@ -0,0 +1,204 @@
+// var num1 =Number(prompt("enter a number"))
+// var num2 =Number(prompt("enter a number"))
+// var sum =num1+num2
+
+// console.log(sum)
+
+// if(sum>=13){
+//     console.log("bar-mitzva")
+// }else {
+//     if(sum >= 12){
+
+//     console.log("bat-mizva")
+
+// }else{
+
+//     console.log("kid")
+// }
+// }
+
+// var number=0
+// while(number <= 100){
+//      console.log(number)
+//     number++
+// }
+
+// var ueserNumber = Number(prompt("enter a number"));
+// var number= 0
+
+// while (0<ueserNumber) {
+//     console.log("no")
+//     number++
+//    
+// }
+
+
+
+
+
+
+
+
+
+//  משימה 3:
+
+// for(var i=0; i<1000; i++){
+//     console.log("javascript is not JAVA")
+
+// }
+
+
+
+
+
+
+// משימה 4:
+
+// var userNumber=Number(prompt("enter a number"))
+// var userName=(prompt("enter a userName"))
+
+// for(var i=0; i<userNumber; i++){
+//     console.log(userName)
+// }
+
+
+
+
+// משימה 5:
+
+// var userName=prompt("enter youur name")
+// var age=Number(prompt("enter your age"))
+// var youremail = prompt("enter your email")
+// var morenumber= prompt("enter num")
+
+
+//     if(age>18){
+//     }for(var i=0 ; i< morenumber ;i++){
+//         console.log(userName ,age , youremail)
+// }
+
+
+
+
+// משימה 6:
+
+// var num1 = Number(prompt("enter a num"))
+// var yourname= "yafit"
+
+// for(var i=0 ; i< num1; i++){
+//      userName =prompt("enter your name")
+//      if(yourname==userName){
+//          console.log("hi boss ")
+//      }
+//      else{
+//          console.log("no entry")
+//      }
+// }
+
+
+
+
+
+
+// משימה 7:
+
+// var num1=Number(prompt("enter firs number"))
+// var num2= Number(prompt("enter sconed number"))
+
+// for(var i=0; i<num1; i++){
+//     for(var j=0 ; j<num2 ; j++){
+//         console.log(i*j)
+//     }
+// }
+
+
+
+
+
+// משימה 8:
+
+// var Numbers=[34,56,76,66,52]
+// console.log(Numbers)
+
+
+
+
+
+// משימה 9: 
+
+// var names=["yafit","roti","eden","lili","david" ]
+// for(var i=0 ; i<names.length; i++)
+//   console.log(names [i])
+
+
+
+
+
+// משימה 10 :
+ 
+// var Numbers=[26,14,24,12,35,4,22,76]
+// for(var i=0 ; i<Numbers.length; i++){
+//   if( Numbers [i] %2 ==0)
+//  console.log(Numbers [i])
+// }
+
+
+
+
+
+
+// משימה 11:
+
+// var yournames=[]
+
+// for(var i= 0; i<5; i++){
+//     yournames [i]=prompt("enter name")
+//     console.log(yournames)
+
+// }
+
+
+
+
+// משימה 12:
+
+var Numbers=[]
+
+for(var i=0 ; i<7; i++){
+    Numbers [i] = Number(prompt("enter A"))
+  if( Numbers [i] %2 !=0)
+ console.log(Numbers )
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
