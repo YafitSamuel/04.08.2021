e2892117be3369d857be68a3e089fe11769d6e22
YafitSamuel <gyafit642@gmail.com>
2021-08-09 11:37:38 +0300

    commit

--- main.js
+++ main.js
@@ -135,7 +135,7 @@
 
 
 // משימה 10 :
- 
+
 // var Numbers=[26,14,24,12,35,4,22,76]
 // for(var i=0 ; i<Numbers.length; i++){
 //   if( Numbers [i] %2 ==0)
@@ -160,45 +160,243 @@
 
 
 
+
+
 // משימה 12:
 
-var Numbers=[]
+// var Numbers=[]
 
-for(var i=0 ; i<7; i++){
-    Numbers [i] = Number(prompt("enter A"))
-  if( Numbers [i] %2 !=0)
- console.log(Numbers )
-}
+// for(var i=0 ; i<7; i++){
+//     Numbers [i] = Number(prompt("enter A"))
+//   if( Numbers [i] %2 !=0)
+//  console.log(Numbers )
+// }
 
 
+
+
+
+
+
+
+// משימה 13:
+
+// var mix= ["yafit" , 12 , "eden" , 67, "koral",91 ]
+//    for(var i=0; i<mix.length; i++){
+//    if(mix [i]*0==0){
+//      console.log ("not a number")
+//         }
+//    }
+//     console.log(mix)
+
+
+
+
+
+
+
+// // משימה 14:
+
+// var usrerNumber = 0 ;
+
+// while (usrerNumber<=0) {
+
+
+//     usrerNumber++
+// }
+
+
+
+
+
+
+
+// משימה 15:
+
+// var num1 =Number(prompt("enter number"))
+// var num2 =Number(prompt("enter number"))
+// var num3 =Number(prompt("enter number"))
+
+
+//     if(num1>num2 && num2>num3){
+//         console.log("סדר יורד")
+
+//     }else if(num3>num2 && num2>num1){
+//        console.log("סדר עולה ")
+//     }
+//    else{
+//        console.log("אין סדר")
+//    }
+
+
+
+
+
+
+
+// --------------משימה לסופש עם אסצאלין---------------
+
+// משימה 1:
+
+// -------א-------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var min = age[0]
+
+
+// for (var i = 1; i < age.length; i++) {
+//     if (age[i] < min) {
+//         min = age[i]
+//     }
+
+// }
+
+//       console.log(min)
+
+
+
+
+
+
+// -------ב--------
+
+    //   var age = [6, 3, 13, 45, 9, 23]
+    //   var max = age[0]
+     
+      
+    //   for (var i = 1; i < age.length; i++) {
+    //       if (age[i] >max) {
+    //         max = age[i]
+    //       }
+
+    //   }
+    //         console.log(max)
+
+
+      
+// ----------ג--------
+
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var sum = 0 ;
+// for (var i = 1; i < age.length; i++) {
+//     sum = sum + age[i];
+// }
+// console.log(sum)
+
+
+
+// --------ד---------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var sum = 0 ;
+// for (var i = 1; i < age.length; i++) {
+//     sum = sum + age[i];
+// }
+// console.log(sum/age.length)
+
+
+
+
+
+// --------ה--------
+
+// var age = [6, 3, 13, 45, 9, 23]
+// var num =Number(prompt("enter name"))
  
 
 
 
 
+// var numbers=[]
+// var sum=0
+// for(var i=0 ; i<5 ;i++){
+//     var num1= Number(prompt("enter number"))
+//     numbers.push(num1)
+//     sum=numbers[i]+sum
+    
+// }
+// console.log(sum)
+// console.log(numbers)
 
 
 
 
+// ------------------------2-------------------
+
+// var nambers = [6, 3, 13, 45, 9, 23]
+
+// var names = [yafit, hila,alona]
 
 
 
+//  ------ -- ----------3----------------------
+
+// var nambers = [6, 13, 18, 45, 76, 23,58,93]
+// var num1 =nambers[1]
+// var num2=nambers[0]
+// nambers[1]=num2
+// nambers[0]=num1
+// var num3 =nambers[7]
+// nambers[7]=num2
+// nambers[0]=num3
+
+// console.log(nambers)
 
 
+ // -----------------4---------------------------
+
+ // var num = [5,3,7,8,11]
+  // var sum = 0 ;
+  // var avg = 0;
+        
+ // for (var i = 0; i < num.length; i++) {
+ //      sum = sum + num[i];
+ // }
+//  console.log(sum)
+   //  console.log(sum/num.length )
+
+      
+  // ----------------------5------------------------
+
+//   var num = ["yafit"," roti ", "keren", "kli" ,"lora"]
+  
+//   for(var i=0; i<5; i++){
+
+//   }
+// console.log(num)
 
 
+// ---------------------6----------------------------
+  
+//   var names = []
+  
+//   for(var i=0; i<5; i++){
+     
+//     names [i]= prompt("enter your name")
+//     console.log(names)
+//   }
 
+// --------------------------7---------------------------
 
+// var names = ["yafit"," roti ", "keren", "kli" ,"lora" , "gog", "lop" , "asa"]
+// for(var i=0 ; i<names.length ;i++){
+// usernname=prompt("enter your name") 
+// names.push(usernname)
+// console.log(names)
 
+// }
+    
+    //  --- ----------------------------8-------------------------------
 
+    var num = []
+    
+    for(var i=0; i<3 ; i++){
+      userNumber [i]=Number(prompt("enter a number"))
+      var kefel= 0;
+      kefel=userNumber*num[i]
+      
+    }
+
+    console.log(kefel)
  
-
-
-
-
-
-
-
-
-
-
